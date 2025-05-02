dce2be7a170ecca88dcf725dffbfdc896c01d776
Bharat Sewani <bharatsewani1993@gmail.com>
2025-05-02 18:18:46 +0530

    allow empty description for org

--- validations/organizationValidations.js
+++ validations/organizationValidations.js
@@ -2,12 +2,12 @@ const joi = require("joi");
 
 const createOrganizationValidations = joi.object().keys({
     name: joi.string().required().max(500),
-    description: joi.string().optional().max(1000),
+    description: joi.string().optional().allow('').max(1000),
 });
 
 const updateOrganizationValidations = joi.object().keys({
     name: joi.string().optional().max(500),
-    description: joi.string().optional().max(1000),
+    description: joi.string().optional().allow('').max(1000),
 });
 
 const deleteOrganizationValidations = joi.object().keys({
